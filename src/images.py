6205ff7d592deb55a93141c90ce2215ea52f7e33
Vinoaj (Vinny) Vijeyakumaar <vinoaj@gmail.com>
2024-10-07 10:00:26 +1100

    init

--- src/images.py
+++ src/images.py
@@ -5,14 +5,35 @@ from .files import get_file_type
 from .config.logger import logger
 
 
-def encode_image(image_path: Path) -> str:
+def encode_image_base64(image_path: Path) -> str:
+    """
+    Encodes an image file to a base64 string.
+
+    Args:
+        image_path (Path): The path to the image file to be encoded.
+
+    Returns:
+        str: The base64 encoded string of the image content.
+    """
     logger.info(f"Encoding image: {image_path}")
     with open(image_path, "rb") as image_file:
         return base64.b64encode(image_file.read()).decode("utf-8")
 
 
-def get_image_encoded_url(image_path: Path) -> str:
-    encoded_image = encode_image(image_path)
+def get_image_base64_encoded_url(image_path: Path) -> str:
+    """
+    Generates a data URL for the given image file.
+
+    Args:
+        image_path (Path): The path to the image file.
+
+    Returns:
+        str: The data URL containing the base64-encoded image and its MIME type.
+
+    Logs:
+        Logs the generated data URL for the given image path.
+    """
+    encoded_image = encode_image_base64(image_path)
     mime_type = get_file_type(image_path)
     data_url = f"data:{mime_type};base64,{encoded_image}"
     logger.info(f"Generated data URL for {image_path}: {data_url}")
