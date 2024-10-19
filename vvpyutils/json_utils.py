599ae7c2f5b9fe7e2296cae16380df2dde0b3100
Vinoaj (Vinny) Vijeyakumaar <vinoaj@gmail.com>
2024-10-20 10:50:02 +1100

    json_utils

---
+++ vvpyutils/json_utils.py
@@ -0,0 +1,20 @@
+import json
+
+
+def json_str_to_dict(json_str: str) -> dict:
+    """
+    Converts a JSON string to a dictionary.
+
+    Args:
+        json_str (str): The JSON string to be converted.
+
+    Returns:
+        dict: The dictionary representation of the JSON string.
+
+    Raises:
+        ValueError: If the JSON string is invalid.
+    """
+    try:
+        return json.loads(json_str)
+    except json.JSONDecodeError as e:
+        raise ValueError(f"Invalid JSON string: {e}") from None
